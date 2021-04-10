829b6cc2b372b9818ba9e8fe6ff5b6140e68daf0
MiguelAngelCuellarS <mcuellar12.leon54@gmail.com>
2021-04-09 22:07:33 -0500

    Create Covid_DC_Sin_Hitos.R

---
+++ Covid_DC_Sin_Hitos.R
@@ -0,0 +1,44 @@
+#limpiar RAM y caché
+gc()
+rm(list=ls())
+
+#Instalar Librerias
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
+#Se cargan las bases de excel desde GitHub Desktop
+##Cargar casos promedio semanales en Buenos Aires
+WDC <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
+                 sheet = "Washington_DC_cases", range = "E2:F60")
+View(WDC)
+
+##Cargar stringency index para Argentina
+stringency_index <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
+                               sheet = "Washington_DC_cases", range = "H2:I464")
+View(stringency_index)
+
+##Cargar hitos legislación y transporte en Argentinas y Buenos Aires
+
+##Cambio de nombres de columnas por practicidad
+colnames(WDC)=c("Semana","Casos")
+colnames(stringency_index)=c("Dia","Index")
+
+#Realizar el Gráfico
+
+##Crear el mapa base
+base_line <- geom_line(data=WDC,aes(Semana,Casos,alpha=0.8))
+
+##Integrar Stringency Index de Oxford
+stringency <- geom_bar(data=stringency_index,stat="identity",aes(Dia,max(WDC$Casos),fill=Index))
+
+##Integrando el gráfico
+ggplot() + stringency + base_line + theme_classic() + scale_fill_viridis_c("Stringency Index
+      Washington D.C.",begin=0.4,end=1,option="magma",aesthetics = "fill") + # escala del mapa de stringency
+  labs(x="Semana",y="Casos semanales promedio",
+       title="Curva de contagios por COVID-19",
+       subtitle="Casos promedio semanales en Washington D.C., EE.UU.",
+       caption=("Fuente de datos: coronavirus.dc.gov")) + ###labels
+  guides(alpha=FALSE)
