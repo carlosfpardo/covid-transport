e3b80f65030b6c404406cadeb29b4d8bce8f0c70
MiguelAngelCuellarS <mcuellar12.leon54@gmail.com>
2021-04-20 00:47:12 -0500

    Update Covid_Buenos_Aires_Sin_Hitos.R

--- Covid_Buenos_Aires_Sin_Hitos.R
+++ Covid_Buenos_Aires_Sin_Hitos.R
@@ -5,9 +5,7 @@ rm(list=ls())
 #Instalar Librerias
 library(ggplot2)
 library(dplyr)
-library(tidyverse)
 library(readxl)
-library(lubridate)
 
 #Se cargan las bases de excel desde GitHub Desktop
 ##Cargar casos promedio semanales en Buenos Aires
@@ -17,17 +15,31 @@ View(BA)
 
 ##Cargar stringency index para Argentina
 stringency_index <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
-                     sheet = "BA_cases", range = "H2:I443")
+                     sheet = "BA_cases", range = "S2:T443")
 View(stringency_index)
 
 ##Cargar hitos legislación y transporte en Argentinas y Buenos Aires
+Hitos <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
+                    sheet = "BA_cases",range = "H2:L19")
+View(Hitos)
 
 ##Cambio de nombres de columnas por practicidad
 colnames(BA)=c("Semana","Casos")
 colnames(stringency_index)=c("Dia","Index")
+colnames(Hitos)=c("Fecha","Clase","Entidad","Hito","Altura")
+
+#Ajustes de de base de hitos dataframe-gráfica
+baseline = min(BA$Casos)
+delta = 0.05 * diff(range(BA$Casos))
+Hitos$ymin = baseline
+Hitos$timelapse = c(diff(Hitos$Fecha),2020-03-01)
+Hitos$bump = Hitos$timelapse < 2*370 #~2 años
+offsets <- rle(Hitos$bump)
+Hitos$offset <- unlist(mapply(function(l,v) {if(v){(l:1)+1}else{rep(1,l)}}, l=offsets$lengths, v=offsets$values, USE.NAMES=FALSE))
+Hitos$ymax <- Hitos$Altura
+Hitos$ymin + Hitos$offset * delta
 
 #Realizar el Gráfico
-
 ##Crear el mapa base
 base_line <- geom_line(data=BA,aes(Semana,Casos,alpha=0.8))
 
@@ -41,4 +53,27 @@ ggplot() + stringency + base_line + theme_classic() + scale_fill_viridis_c("Stri
        title="Curva de contagios por COVID-19",
        subtitle="Casos promedio semanales en Buenos Aires, Argentina",
        caption=("Fuente de datos: buenosaires.gob.ar")) + ###labels
+  scale_y_continuous(limits=c(-3150,4250)) + # change y axis scale
+  geom_segment(data = Hitos, mapping=aes(x=Fecha, y=ymin, xend=Fecha, yend=ymax)) + ###Agregar lineas de Hitos
+  geom_point(data = Hitos, mapping=aes(x=Fecha,y=ymax), size=1.2) + ###Agregar puntos de hitos
+  geom_text(data = Hitos, mapping=aes(x=Fecha, y=ymax, label=Hito), hjust=-0.01, vjust=0.5, size=2.8)+ ###Agregar leyendas de hitos
   guides(alpha=FALSE)
+
+##Ahora con google mobility
+###Cargando la base
+Google_mob <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
+                         sheet = "BA_cases", range = "S2:U454", col_types=c("date","skip","numeric"))
+View(Google_mob)
+
+###Integrar el indice de restricción de movilidad de Google para el caso de tiendas
+mobility_index <- geom_bar(data=Google_mob,stat="identity",aes(Dia,max(BA$Casos),fill=Google_index))
+
+###Integrando el gráfico
+ggplot() + mobility_index + base_line + theme_classic() + scale_fill_viridis_c("Google mobility Index
+        Buenos Aires",begin=0.4,end=1,option="magma",aesthetics = "fill") + # escala del mapa de stringency
+  labs(x="Semana",y="Casos semanales promedio",
+       title="Curva de contagios por COVID-19",
+       subtitle="Casos promedio semanales en Buenos Aires, Argentina",
+       caption=("Fuente de datos: buenosaires.gob.ar")) + ###labels
+  guides(alpha=FALSE)
+
