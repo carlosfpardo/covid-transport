6b544b038aaadc950749be53caa588bc3d80e0e7
MiguelAngelCuellarS <mcuellar12.leon54@gmail.com>
2021-04-20 00:47:15 -0500

    Update Covid_Bogotá_Hitos.R

--- Covid_Bogotá_Hitos.R
+++ Covid_Bogotá_Hitos.R
@@ -5,19 +5,17 @@ rm(list=ls())
 #Instalar Librerias
 library(ggplot2)
 library(dplyr)
-library(tidyverse)
 library(readxl)
-library(lubridate)
 
 #Se cargan las bases de excel desde GitHub Desktop
 ##Cargar casos promedio semanales
 Bog <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
-                  range = "E2:F58")
+                  range = "E2:F60")
 View(Bog)
 
 ##Cargar stringency index para Colombia
 stringency_index <- read_excel("Bogota_daily_cases.xlsx", 
-                                 range = "S2:T428")
+                                 range = "S2:T442")
 View(stringency_index)
 
 ##Cargar hitos legislación y transporte en Colombia
@@ -39,11 +37,9 @@ Hitos$bump = Hitos$timelapse < 2*370 #~2 años
 offsets <- rle(Hitos$bump)
 Hitos$offset <- unlist(mapply(function(l,v) {if(v){(l:1)+1}else{rep(1,l)}}, l=offsets$lengths, v=offsets$values, USE.NAMES=FALSE))
 Hitos$ymax <- Hitos$Altura
-
 Hitos$ymin + Hitos$offset * delta
 
 #Realizar el Gráfico
-
 ##Crear el mapa base
 base_line <- geom_line(data=Bog,aes(Semana,Casos,alpha=0.8))
 
@@ -64,19 +60,28 @@ ggplot() + stringency + base_line + theme_classic() + scale_fill_viridis_c("Stri
   geom_text(data = Hitos, mapping=aes(x=Fecha, y=ymax, label=Hito), hjust=-0.01, vjust=0.5, size=2.8)+ ###Agregar leyendas de hitos
   guides(alpha=FALSE)
 
-##Integrando el gráfico
-#() + stringency + base_line + theme_classic() + scale_fill_viridis_c("Stringency Index
-#      Colombia",begin=0.4,end=1,option="magma",aesthetics = "fill") + # escala del mapa de stringency
-#  labs(x="Semana",y="Casos semanales promedio",
-#       title="Curva de contagios por COVID-19",
-#       subtitle="Casos promedio semanales en Bogotá, Colombia",
-#       caption=("Fuente de datos: saludata.saludcapital.gov.co")) + ###labels
-#  #scale_x_continuous(breaks = c(1:14),labels = c("Marzo 2020","Abril 2020","Mayo 2020","Junio 2020","Julio 2020","Agosto 2020","Septiembre 2020","Octubre 2020","Noviembre 2020","Diciembre 2020","Enero 2021","Febrero 2021","Marzo 2021","Abril 2021"))+
-#  scale_y_continuous(limits=c(-4300,5400)) + # change y axis scale
-#  geom_segment(data = Hitos, mapping=aes(x=Fecha, y=ymin, xend=Fecha, yend=ymax)) + ###Agregar lineas de Hitos
-#  geom_point(data = Hitos, mapping=aes(x=Fecha,y=ymax, col=Clase), size=1.2) + ###Agregar puntos de hitos
-#  geom_text(data = Hitos, mapping=aes(x=Fecha, y=ymax, label=Hito), hjust=-0.01, vjust=0.5, size=2.8)+ ###Agregar leyendas de hitos
-#  guides(alpha=FALSE)
+##Ahora con google mobility
+###Cargando la base
+Google_mob <- read_excel("~/GitHub/covid-transport/Bogota_daily_cases.xlsx", 
+                  range = "S2:U448", col_types=c("date","skip","numeric"))
+View(Google_mob)
+
+###Integrar el indice de restricción de movilidad de Google para el caso de tiendas
+mobility_index <- geom_bar(data=Google_mob,stat="identity",aes(Dia,max(Bog$Casos),fill=Google_index))
+
+###Integrando el gráfico
+ggplot() + mobility_index + base_line + theme_classic() + scale_fill_viridis_c("Google mobility Index
+        Bogotá",begin=0.4,end=1,option="magma",aesthetics = "fill") + # escala del mapa de stringency
+  labs(x="Semana",y="Casos semanales promedio",
+       title="Curva de contagios por COVID-19",
+       subtitle="Casos promedio semanales en Bogotá, Colombia",
+       caption=("Fuente de datos: saludata.saludcapital.gov.co")) + ###labels
+  #scale_x_date(date_labels="%b %y",date_breaks = "1 month",limits=c(as.Date("2020-03-01"),as.Date("2021-05-01")))+
+  scale_y_continuous(limits=c(-4300,5400)) + # change y axis scale
+  geom_segment(data = Hitos, mapping=aes(x=Fecha, y=ymin, xend=Fecha, yend=ymax)) + ###Agregar lineas de Hitos
+  geom_point(data = Hitos, mapping=aes(x=Fecha,y=ymax), size=1.2) + ###Agregar puntos de hitos
+  geom_text(data = Hitos, mapping=aes(x=Fecha, y=ymax, label=Hito), hjust=-0.01, vjust=0.5, size=2.8)+ ###Agregar leyendas de hitos
+  guides(alpha=FALSE)
 
 ##Grafica preliminar de casos semanales
 ###Solo borrador de curva general
@@ -90,5 +95,4 @@ ggplot(Bog, aes(x=Semana)) +
   scale_x_date(date_labels="%b %y",date_breaks = "1 month",limits=c(as.Date("2020-03-01"),as.Date("2021-05-01")))+
   theme(axis.text.x = element_text(angle = 90, vjust=0.5, size = 8), 
         panel.grid.minor = element_blank())
-
 #scale_color_gradient
